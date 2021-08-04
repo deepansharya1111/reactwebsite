434ad176df01db728631e3c87822628a8a1f0369
deepanshary1111 <deepanshary1111@gmail.com>
2021-08-05 01:52:30 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactwebsite/precache-manifest.b2c37a4ad62ba48b8943a2957f31cb11.js"
+  "/reactwebsite/precache-manifest.a807037b88f08fbf04305db4b2c4bbd2.js"
 );
 
 self.addEventListener('message', (event) => {
