1aa4f04aeebb28027b9c4ccbb00cdf3f4951fd65
deepanshary1111 <deepanshary1111@gmail.com>
2021-08-04 23:34:45 +0530

    Updates

---
+++ precache-manifest.fb634613ca246fe4f804825e0f7bd21f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c73fddc10d288263689c31298d8744",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "1975520a72c573938141",
+    "url": "/reactwebsite/static/css/2.0520b0af.chunk.css"
+  },
+  {
+    "revision": "607a619a85192f7ab67a",
+    "url": "/reactwebsite/static/css/main.16516185.chunk.css"
+  },
+  {
+    "revision": "1975520a72c573938141",
+    "url": "/reactwebsite/static/js/2.dac086b6.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.dac086b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607a619a85192f7ab67a",
+    "url": "/reactwebsite/static/js/main.33b0c0f7.chunk.js"
+  },
+  {
+    "revision": "eed1bdfc10bb96feed2a",
+    "url": "/reactwebsite/static/js/runtime-main.a27c2035.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactwebsite/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactwebsite/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactwebsite/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactwebsite/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebsite/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactwebsite/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactwebsite/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactwebsite/static/media/software.43049cae.jpeg"
+  }
+]);
